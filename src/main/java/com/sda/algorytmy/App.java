068fc849b5498bb5e263658c83156d07377c456e
Mateusz Erdman/Matek <mateusz.erdman@hotmail.com>
2019-05-12 14:38:27 +0100

    Element ktory sie powtarza

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,5 +1,7 @@
 package com.sda.algorytmy;
 
+import java.util.Random;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       int[] a = {5,1,2,3,1,6,1};
+       int wynik = 0;
+       int x = 1;
+
+    for(int i = 1; i < a.length; i++){
+       if (a[i] == 1){
+           wynik = wynik +1;
+            }
+        }
+        System.out.println(wynik);
     }
-}
+
+    }
+
+
+
+
+
